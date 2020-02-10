d3e7eaf3791ece25ce7e58e265f4f10963e32ce7
Ivan <fivanunam@gmail.com>
2020-02-10 09:08:31 -0600

    add the corresponding methods to Code class

--- lib/code.rb
+++ lib/code.rb
@@ -1,3 +1,20 @@
 class Code
-  
+  attr_reader :file_to_string, :symbolic_expression_tree
+  def initialize(path)
+    @file_to_string = File.read(path)
+    @symbolic_expression_tree = Ripper.sexp(@file_to_string)
+  end
+
+  def find(sym, matched_arrays, array = @symbolic_expression_tree)
+    if array[0] == sym
+      matched_arrays.push(array)
+      return matched_arrays
+    end
+
+    array.each do |item|
+      if item.is_a?(Array)
+        find(sym, matched_arrays, item)
+      end
+    end
+  end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -1,18 +1,3 @@
 class Parser
-  def self.code(code_as_string)
-    Ripper.sexp(code_as_string)
-  end
 
-  def self.find(sym, array, matched_arrays)
-    if array[0] == sym
-      matched_arrays.push(array)
-      return matched_arrays
-    end
-
-    array.each do |item|
-      if item.is_a?(Array)
-        find(sym, item, matched_arrays)
-      end
-    end
-  end
 end
