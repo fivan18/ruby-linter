a29249c16fd8486b002decee286cb6c6fd203446
Ivan <fivanunam@gmail.com>
2020-02-09 18:19:20 -0600

    add find method to find arrays based on a symbol

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,18 @@
 class Parser
   def self.code(code_as_string)
-    pp Ripper.parse(code_as_string)
+    Ripper.sexp(code_as_string)
+  end
+
+  def self.find(sym, array, matched_arrays)
+    if array[0] == sym
+      matched_arrays.push(array)
+      return matched_arrays
+    end
+
+    array.each do |item|
+      if item.is_a?(Array)
+        find(sym, item, matched_arrays)
+      end
+    end
   end
 end
