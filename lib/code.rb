e14b968a09029b45f4466a94838fbd99b8e0b919
Ivan <fivanunam@gmail.com>
2020-02-10 11:58:32 -0600

    add #bad_identifiers instance method

--- lib/code.rb
+++ lib/code.rb
@@ -1,7 +1,11 @@
+require 'ripper'
+
 class Code
   attr_reader :file_to_string, :symbolic_expression_tree
-  def initialize(path)
-    @file_to_string = File.read(path)
+  
+  def initialize(file)
+    puts Dir.pwd
+    @file_to_string = File.read(Dir.pwd + '/spec/test_code/' + file)
     @symbolic_expression_tree = Ripper.sexp(@file_to_string)
   end
 
@@ -16,5 +20,23 @@ class Code
         find(sym, matched_arrays, item)
       end
     end
+  end
+
+  def bad_identifiers
+    # [:assign, [:var_field, [:@ident, "заплата", [5, 2]]], [:@int, "1_000", [5, 19]]]
+    assignations = []
+    find(:assign, assignations)
+
+    bad_identifiers_arr = []
+    assignations.each do |assign|
+      # [:var_field, [:@ident, "заплата", [5, 2]]]
+      var_field = assign[1]
+
+      unless var_field[1][1].ascii_only?
+        bad = [var_field[1][1], var_field[1][2]]
+        bad_identifiers_arr.push(bad)
+      end
+    end
+    bad_identifiers_arr
   end
 end
