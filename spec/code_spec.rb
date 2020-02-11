7da2feaf2634163623aa452cbd9adc0f871e160f
Ivan <fivanunam@gmail.com>
2020-02-11 10:52:10 -0600

    refactor

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe Code do
-  let(:code) { Code.new('test_code.rb') }
+  let(:code) { Code.new(Dir.pwd + '/spec/test/test_script.rb') }
 
-  describe '#bad_identifiers' do
-    it 'returns indetifiers that are not ascii-only' do
-      expect(code.bad_identifiers).to eql([['заплата', [5, 2]]])
+  describe '#variables' do
+    it 'returns all names of variables asignations' do
+      expect(code.variables).to eql([['заплата', [5, 2]], ['salary', [8, 2]]])
     end
   end
 end
