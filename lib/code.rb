7cfd8b6451c309487d00babcc8525d3b47cc080f
Ivan <fivanunam@gmail.com>
2020-02-11 10:50:15 -0600

    The class will only provide the names of methods, variables, symbols and classes.Added vairiables class.

--- lib/code.rb
+++ lib/code.rb
@@ -8,6 +8,33 @@ class Code
     @symbolic_expression_tree = Ripper.sexp(@file_to_string)
   end
 
+  def variables
+    # [:assign, [:var_field, [:@ident, "заплата", [5, 2]]], [:@int, "1_000", [5, 19]]]
+    assignations = []
+    find(:assign, assignations)
+
+    variables = []
+    assignations.each do |assign|
+      # [:var_field, [:@ident, "заплата", [5, 2]]]
+      var_field = assign[1]
+
+      variable = [var_field[1][1], var_field[1][2]]
+      variables.push(variable)
+    end
+    variables
+  end
+
+  def symbols
+
+  end
+
+  def methods
+  end
+
+  def classes
+  end
+
+  private
   def find(sym, matched_arrays, array = @symbolic_expression_tree)
     if array[0] == sym
       matched_arrays.push(array)
@@ -19,23 +46,5 @@ class Code
         find(sym, matched_arrays, item)
       end
     end
-  end
-
-  def bad_identifiers
-    # [:assign, [:var_field, [:@ident, "заплата", [5, 2]]], [:@int, "1_000", [5, 19]]]
-    assignations = []
-    find(:assign, assignations)
-
-    bad_identifiers_arr = []
-    assignations.each do |assign|
-      # [:var_field, [:@ident, "заплата", [5, 2]]]
-      var_field = assign[1]
-
-      unless var_field[1][1].ascii_only?
-        bad = [var_field[1][1], var_field[1][2]]
-        bad_identifiers_arr.push(bad)
-      end
-    end
-    bad_identifiers_arr
   end
 end
