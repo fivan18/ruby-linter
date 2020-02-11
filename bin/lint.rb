b0664d7c45bdabc5ef38eaaeaca9df36c0888595
Ivan <fivanunam@gmail.com>
2020-02-10 22:50:21 -0600

    add display method

--- bin/lint.rb
+++ bin/lint.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'tty-prompt'
 require_relative '../lib/manager.rb'
+
+def display(symbol, no_valid)
+  prompt = TTY::Prompt.new
+
+  if no_valid.empty?
+    prompt.ok(VALIDATIONS[symbol] + '=========> No offences')
+  else
+    prompt.error(VALIDATIONS[symbol])
+    no_valid.each do |item|
+      prompt.warn("      #{item[0]} line #{item[1][0]} column #{item[1][1]}")
+    end
+  end
+end
 
 SYMBOL_VALIDATIONS = [:validation1]
 
@@ -12,5 +26,5 @@ VALIDATIONS = {
 path_file = Dir.pwd + '/' + ARGV[0]
 manager = Manager.new(path_file)
 
-pp manager.with_no_conventions(:validation1)
+display(:validation1, manager.with_no_conventions(:validation1))
 
