efbc233ecb8bd378259bafd9dc8a0243f622aa9c
Ivan <fivanunam@gmail.com>
2020-02-10 21:58:16 -0600

    basic user script

--- bin/lint.rb
+++ bin/lint.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-puts Dir.pwd
+require_relative '../lib/manager.rb'
+
+SYMBOL_VALIDATIONS = [:validation1]
+
+VALIDATIONS = {
+  :validation1 => 'no-ascii identifiers'
+}
+
+path_file = Dir.pwd + '/' + ARGV[0]
+manager = Manager.new(path_file)
+
+pp manager.with_no_conventions(:validation1)
+
