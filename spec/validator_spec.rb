edb1095c6d72fb3138ffffa44882e1a89fdc6c9c
Ivan <fivanunam@gmail.com>
2020-02-12 02:39:51 -0600

    fix test cases

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe Validator do
-  let(:validator) { Validator.new(Dir.pwd + '/spec/test/test_script.rb') }
+  let(:validator) { Validator.new(Dir.pwd + '/spec/test/test_script') }
 
   describe '#validate' do
     it 'returns all non ascii variable names' do
-      expect(validator.validate(:variables, :ascii)).to eql([['заплата', [5, 2]]])
+      expect(validator.validate(:variables, :ascii)).to eql([['заплата', [3, 2]]])
     end
 
     it 'returns all non snakecase variable names' do
