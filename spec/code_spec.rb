49a7c865027cbf4db06e652365a99b9d9ceebc75
Ivan <fivanunam@gmail.com>
2020-02-14 14:14:02 -0600

    upgrade test cases according to classes_test_script and test_script files

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe Code do
 
   describe '#variables' do
     it 'returns variables names from test_script' do
-      expect(code.variables).to eql([['CONSTANT',[6, 2]],
-                                     ['good_variable',[15, 4]],
-                                     ['заплата',[20, 4]],
-                                     ['salary',[21, 4]],
+      expect(code.variables).to eql([['CONSTANT', [6, 2]],
+                                     ['good_variable', [15, 4]],
+                                     ['заплата', [20, 4]],
+                                     ['salary', [21, 4]],
                                      ['good_variable', [23, 4]]])
     end
   end
 
   describe '#symbols' do
     it 'returns symbols names from test_script' do
-      expect(code.symbols).to eql([['baп_symbol',[3, 15]],
-                                   ['BabSymbol',[3, 29]],
-                                   ['good_symbol',[4, 15]],
-                                   ['BADSYMBOL',[15, 23]]])
+      expect(code.symbols).to eql([['baп_symbol', [3, 15]],
+                                   ['BabSymbol', [3, 29]],
+                                   ['good_symbol', [4, 15]],
+                                   ['BADSYMBOL', [15, 23]]])
     end
   end
 
@@ -35,9 +35,9 @@ RSpec.describe Code do
 
   describe '#methods' do
     it 'returns methods names from test_script' do
-      expect(code.methods).to eql([['a_Method',[9, 6]],
-                                   ['anotherMethod',[14, 6]],
-                                   ['god_method',[18, 6]]])
+      expect(code.methods).to eql([['a_Method', [9, 6]],
+                                   ['anotherMethod', [14, 6]],
+                                   ['god_method', [18, 6]]])
     end
   end
 end
